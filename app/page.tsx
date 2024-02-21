2c8c1cd90fb16ac4f0d4dce2fbbc2eb56637c2c2
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-02-21 22:03:58 +0530

    css changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ async function Home() {
 
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-      <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+      <h2 className="text-2xl text-white font-bold">Explore Anime</h2>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data.map((item: AnimeProp, index: number) => (
